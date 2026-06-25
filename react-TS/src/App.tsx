cb693a2e81ee9dd0f996d04f7819b0760c9f2e1b
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2026-06-25 20:11:31 -0300

    props react

--- react-TS/src/App.tsx
+++ react-TS/src/App.tsx
@@ -1,7 +1,28 @@
+
+interface ICardProps {
+  title: string,
+  children?: React.ReactNode
+}
+
+const Card = (props: ICardProps) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+
+      <p>{props.children}</p>
+
+      <div>Footer</div>
+    </div>
+  )
+}
+
 export function App() {
   return (
     <>
       <p>Olá</p>
+      <Card title="Meu Card">
+        <p>Este é o conteúdo do card.</p>
+      </Card>
     </>
   )
 }
